0f840159f81fc6914c7ff98ba76ee382b8fdee89
Ryan <rtg5xkh@virginia.edu>
2022-10-22 13:53:56 -0400

    pool progress

--- pool/pool.cc
+++ pool/pool.cc
@@ -1,4 +1,5 @@
 #include "pool.h"
+using namespace std;
 
 Task::Task() {
 }
@@ -6,14 +7,84 @@ Task::Task() {
 Task::~Task() {
 }
 
+void ThreadPool::RunThread() {
+    while (true) {
+        pthread_mutex_lock(&queue_lock);
+        while (task_queue.empty()) {
+            pthread_cond_wait(&task_ready, &queue_lock);
+            pthread_mutex_lock(&stop_lock);
+            if (stop) {
+                pthread_mutex_unlock(&stop_lock);
+                pthread_mutex_unlock(&queue_lock);
+                cout << "Thread " << getpid() << " Finished\n";
+                return;
+            }
+            pthread_mutex_unlock(&stop_lock);
+        }
+        Task *task = task_queue.front();
+        task_queue.pop();
+        pthread_mutex_unlock(&queue_lock);
+        task->Run();
+        pthread_mutex_lock(&(task->done_lock));
+        task->done = true;
+        pthread_cond_broadcast(&(task->done_cv));
+        pthread_mutex_unlock(&(task->done_lock));
+    }
+}
+
+void *run_thread_helper(void *instance) {
+    ThreadPool* tptr = (ThreadPool*)instance;
+    tptr->RunThread();
+    return NULL;
+}
+
 ThreadPool::ThreadPool(int num_threads) {
+    pthread_mutex_init(&stop_lock, NULL);
+    pthread_mutex_init(&queue_lock, NULL);
+    pthread_mutex_init(&map_lock, NULL);
+    pthread_cond_init(&task_ready, NULL);
+    for (int i = 0; i < num_threads; i++) {
+        pthread_t thread;
+        pthread_create(&thread, NULL, run_thread_helper, (void*)this);
+        pthread_join(thread, NULL);
+    }
 }
 
 void ThreadPool::SubmitTask(const std::string &name, Task* task) {
+    pthread_mutex_init(&(task->done_lock), NULL);
+    pthread_cond_init(&(task->done_cv), NULL);
+    
+    pthread_mutex_lock(&(task->done_lock));
+    task->done = false;
+    pthread_mutex_unlock(&(task->done_lock));
+
+    pthread_mutex_lock(&map_lock);
+    task_map[name] = task;
+    pthread_mutex_unlock(&map_lock);
+
+    pthread_mutex_lock(&queue_lock);
+    task_queue.push(task);
+    pthread_cond_signal(&task_ready);
+    pthread_mutex_unlock(&queue_lock);
 }
 
 void ThreadPool::WaitForTask(const std::string &name) {
+    pthread_mutex_lock(&map_lock);
+    Task *task = task_map[name];
+    pthread_mutex_unlock(&map_lock);
+
+    pthread_mutex_lock(&task->done_lock);
+    while (!task->done) {
+        pthread_cond_wait(&(task->done_cv), &(task->done_lock));
+    }
+    delete task;
+    pthread_mutex_unlock(&task->done_lock);
+    
 }
 
 void ThreadPool::Stop() {
+    pthread_mutex_lock(&stop_lock);
+    stop = true;
+    pthread_cond_broadcast(&task_ready);
+    pthread_mutex_unlock(&stop_lock);
 }
--- pool/pool.h
+++ pool/pool.h
@@ -1,10 +1,21 @@
 #ifndef POOL_H_
+#include <iostream>
+#include <unistd.h>
 #include <string>
 #include <pthread.h>
+#include <queue>
+#include <unordered_map>
+using namespace std;
 
 class Task {
 public:
+
+    bool done;
+    pthread_cond_t done_cv;
+    pthread_mutex_t done_lock;
+
     Task();
+
     virtual ~Task();
 
     virtual void Run() = 0;  // implemented by subclass
@@ -12,7 +23,21 @@ public:
 
 class ThreadPool {
 public:
+
+    bool stop = false;
+    pthread_mutex_t stop_lock;
+
+    queue<Task*> task_queue;
+    pthread_mutex_t queue_lock;
+
+    unordered_map<string, Task*> task_map;
+    pthread_mutex_t map_lock;
+
+    pthread_cond_t task_ready;
+
     ThreadPool(int num_threads);
+
+    void RunThread();
 
     // Submit a task with a particular name.
     void SubmitTask(const std::string &name, Task *task);
